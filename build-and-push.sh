36620e90dbfbe9323435441a5056485481b91373
EC2 Default User <ec2-user@ip-172-16-90-51.ec2.internal>
2020-11-09 05:20:30 +0000

    refactor and add comments

--- build-and-push.sh
+++ build-and-push.sh
@@ -4,21 +4,21 @@
 algorithm_name=yolo-tensorflow
 
 cd container
-
+# get information - account and region, required by ECR https://aws.amazon.com/ecr/
 account=$(aws sts get-caller-identity --query Account --output text)
-
+echo $account
 # Get the region defined in the current configuration (default to us-west-2 if none defined)
 region=$(aws configure get region)
 region=${region:-us-west-2}
 echo $region
 
 
+# derive fullname of docker image 
 fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
 
 # If the repository doesn't exist in ECR, create it.
 
 aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
-
 if [ $? -ne 0 ]
 then
     aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
@@ -27,15 +27,15 @@ fi
 # Get the login command from ECR and execute it directly
 $(aws ecr get-login --region ${region} --no-include-email)
 
-# Get the login command from ECR in order to pull down the SageMaker PyTorch image
+# Get the login command from ECR in order to pull down the SageMaker TensorFlow image
 $(aws ecr get-login --registry-ids 763104351884 --region ${region} --no-include-email)
 
-# Build the docker image locally with the image name and then push it to ECR
-# with the full name.
+# get the fullname of deep learning container image 
 base_img='763104351884.dkr.ecr.'$region'.amazonaws.com/tensorflow-training:1.15.2-gpu-py36-cu100-ubuntu18.04'
 echo 'base_img:'$base_img
 
+# Build the docker image locally with the image name and then push it to ECR
+# with the full name.
 docker build  -t ${algorithm_name} -f Dockerfile  --build-arg BASE_IMG="${base_img}" .  
 docker tag ${algorithm_name} ${fullname}
-
 docker push ${fullname}
